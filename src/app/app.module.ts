8a9716c6b08d46564cb74a3a8a0a583296eddfbc
Manuel Reiländer <manuel.reilaender@dimetor.com>
2018-04-10 10:51:05 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularCesiumModule } from 'angular-cesium';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularCesiumModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
